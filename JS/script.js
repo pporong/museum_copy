4e985dab7a95a7c2ff7c153a7998fa40a3c9c3b4
pporong <inane09@gmail.com>
2022-04-14 18:29:02 +0900

    변경내용 메모

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,20 @@
 $(document).ready(function(){
 
+
+    // 메인 슬라이드
     $('.all-wrap >.main-visual >.main-slider').slick({
-        
+        autoplay: false ,
+        autoplaySpeed : 2000 , 
+        dots: false ,
+        pauseOnHover: false ,
+        arrows : false ,
     })
+    // 메인 슬라이드 버튼
+    $('.all-wrap >.main-visual .slider-btn-box >.prev-btn').click(function(){
+        $('.main-slider').slick('slickPrev')
+    });
+    $('.all-wrap >.main-visual .slider-btn-box >.next-btn').click(function(){
+        $('.main-slider').slick('slickNext')
+    });
     
 });
