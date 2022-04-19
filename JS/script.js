902c6431ae6b6e6acc16f1533a3ab9992c096d25
pporong <inane09@gmail.com>
2022-04-19 21:52:51 +0900

    섹션 3만들기 해야함

--- JS/script.js
+++ JS/script.js
@@ -1,10 +1,16 @@
 $(document).ready(function(){
+
     // 현재 슬라이드 닷츠
 
-    // init slide change
+    // init slide change : 메인슬라이드
   $('.all-wrap >.main-visual >.main-slider').on('init', function(event, slick){
     $('.slider-dots-box >.slider-dots >.dots').eq(0).addClass('active');
     $('.main-visual >.slider-dots-box .control-btn-box >.pause').addClass('active')
+  });
+
+    // init slide change : 서브슬라이드
+  $('.all-wrap >.section02 >.section02-post').on('init', function(event, slick){
+    $('.all-wrap >.section02 >.section02-title >.title-navi .pause').addClass('active')
   });
   
 
@@ -18,10 +24,10 @@ $(document).ready(function(){
     })
     // 메인 슬라이드 버튼
     $('.all-wrap >.main-visual .slider-btn-box >.prev-btn').click(function(){
-        $('.main-slider').slick('slickPrev')
+        $('.all-wrap >.main-visual >.main-slider').slick('slickPrev')
     });
     $('.all-wrap >.main-visual .slider-btn-box >.next-btn').click(function(){
-        $('.main-slider').slick('slickNext')
+        $('.all-wrap >.main-visual >.main-slider').slick('slickNext')
     });
 
     // 메인 슬라이드 닷츠
@@ -51,6 +57,40 @@ $(document).ready(function(){
         $('.main-visual >.slider-dots-box .control-btn-box >.play').addClass('active')
         $('.main-visual >.slider-dots-box .control-btn-box >.pause').removeClass('active')
     });
+
+
+    // 섹션02 ; 서브슬라이드 (전시 섹션)
+    $('.all-wrap >.section02 >.section02-post').slick({
+        slidesToShow: 4,
+        swipeToSlide : true ,
+        dots: false ,
+        arrows : false ,
+        autoplay : true ,
+        autoplaySpeed : 2000 , 
+
+    })
+    // 서브 슬라이드 버튼
+    $('.all-wrap >.section02 >.section02-title >.title-navi .prev-btn').click(function(){
+        $('.all-wrap >.section02 >.section02-post').slick('slickPrev')
+    });
+    $('.all-wrap >.section02 >.section02-title >.title-navi .next-btn').click(function(){
+        $('.all-wrap >.section02 >.section02-post').slick('slickNext')
+    });
+
+    // 서브 슬라이드 컨트롤 버튼
+    $('.all-wrap >.section02 >.section02-title >.title-navi .play').click(function(){
+        $('.all-wrap >.section02 >.section02-post').slick("slickPlay")
+        $('.all-wrap >.section02 >.section02-title >.title-navi .play').removeClass('active')
+        $('.all-wrap >.section02 >.section02-title >.title-navi .pause').addClass('active')
+    });
+    $('.all-wrap >.section02 >.section02-title >.title-navi .pause').click(function(){
+        $('.all-wrap >.section02 >.section02-post').slick("slickPause")
+        $('.all-wrap >.section02 >.section02-title >.title-navi .play').addClass('active')
+        $('.all-wrap >.section02 >.section02-title >.title-navi .pause').removeClass('active')
+    });
+
+
+
 
 
 });
